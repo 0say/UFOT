d56bc2449d62fde73271e36250870acffcde256b
0say <dragondeoz@outlook.com>
2024-03-17 19:11:15 -0400

    v
    ton

--- UFOT/obj/Debug/net8.0/UFOT.AssemblyInfo.cs
+++ UFOT/obj/Debug/net8.0/UFOT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UFOT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62adf6efa72082f25aebfe833e74aa968aa35176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84371cd71ed4aa5c6beca9c08c6d660c134a11cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("UFOT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UFOT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
